3f9b3ecb95024eefb8b4e6acf78b674c1ec408ec
xinoHITO <hitoshi_kamisato@hotmail.com>
2018-02-06 21:56:25 -0500

    modulo 4 clase 1   06.02.18
    Primer push de modulo de 3D
    
    Importar modelos 3D y sus animaciones
    Personaje 3d con animaciones de idle,caminar y correr.
    El personaje se mueve hacia adelante ya hacia atrás y si presionas shift ... camina

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+	public float _walkSpeed = 2;
+	public float _runSpeed = 5;
+	private Animator _animator;
+	private Rigidbody _rigidbody;
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponent<Animator> ();
+		_rigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float v = Input.GetAxis ("Vertical");
+
+		Vector3 moveVector = new Vector3 (0, 0, v);
+
+	
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			moveVector *= _walkSpeed;
+		} else {
+			moveVector *= _runSpeed;
+		}
+
+		_rigidbody.velocity = moveVector;
+
+		transform.LookAt (transform.position + moveVector);
+
+
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			_animator.SetFloat ("speed", Mathf.Abs(v));
+		} else {
+			_animator.SetFloat ("speed", Mathf.Abs(v)*2);
+		}
+	}
+}
